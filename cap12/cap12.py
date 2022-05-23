ac8c96d6b8e2f776a18cbcaeb6fdb49c9fbb5180
SaidJun10r <said_junior2015@outlook.com>
2022-05-23 00:26:09 -0300

    mudanças capitulo 12

---
+++ cap12/cap12.py
@@ -0,0 +1,10 @@
+# Capitulo 12 Livro
+
+# Importando bibliotecas
+from statistics import variance
+from skimage import io, data
+from matplotlib import pyplot as plt
+import numpy as np
+
+# Importando a imagem no código
+img = io.imread('cap12/teste.jpg')
