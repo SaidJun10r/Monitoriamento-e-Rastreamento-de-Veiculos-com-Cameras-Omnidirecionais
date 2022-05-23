b494eb7a7d8ff24c98d081ecffa651034e4e596f
SaidJun10r <said_junior2015@outlook.com>
2022-05-23 00:31:42 -0300

    23/05/2022

--- cap12/cap12.py
+++ cap12/cap12.py
@@ -8,3 +8,14 @@ import numpy as np
 
 # Importando a imagem no código
 img = io.imread('cap12/teste.jpg')
+grayimg = io.imread('cap3/teste.jpg', as_gray=True) # Imagem em grayscale
+
+# Mostrando as imagens
+plt.subplot(1,2,1)
+plt.title("imagem Original")
+plt.imshow(img)
+plt.subplot(1,2,2)
+plt.title("Imagem em escala de cinza")
+plt.imshow(grayimg, cmap='gray')
+
+plt.show()
